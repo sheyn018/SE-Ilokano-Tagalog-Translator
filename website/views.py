b79ce1b9c6f81d51c6308985081d3a4b267aa347
rupihongseokkie <ruffy.rupi@gmail.com>
2023-02-15 16:12:17 +0800

    edited views.py for the system tester

--- website/views.py
+++ website/views.py
@@ -2,6 +2,8 @@ import pandas as pd
 from flask import Blueprint, render_template, request
 from module.tl_il.doc_trans_smt_tl import tl_smt_trans
 from module.il_tl.doc_trans_smt_il import il_smt_trans
+from module.scoring import scoring_bleu
+from module.scoring import ter
 from flask import Blueprint, render_template, request
 import pandas as pd
 
@@ -49,14 +51,40 @@ def system_tester_tg_il():
     """
     Route for the page that handles the system tester for Tagalog to Ilokano machine translation
     """
-    return render_template('system_tester_tg-il.html')
+    source = ''
+    expected = ''
+    op_sen_list = []
+    if request.method == 'POST':
+        source = request.form.get('source_lang')
+        dict_tl_il_result = tl_smt_trans(source)
+        dict_tl_il_result = pd.DataFrame(dict_tl_il_result)
+        op_sen_list = dict_tl_il_result['System Output'].tolist()
+        expected = request.form. get('expected_out')
+        expected = dict_tl_il_result['Expected Output'].tolist()
+        ave_bleu = scoring_bleu(dict_tl_il_result)
+        ave_ter = ter(dict_tl_il_result)
+    
+    return render_template('system_tester_tg-il.html', source=source, op_sen_list=op_sen_list, expected=expected, ave_bleu=ave_bleu, ave_ter=ave_ter)
 
 @views.route('/system_tester_il_tg')
 def system_tester_il_tg():
     """
     Route for the page that handles the system tester for Ilokano to Tagalog machine translation
     """
-    return render_template('system_tester_il-tg.html')
+    source = ''
+    expected = ''
+    op_sen_list = []
+    if request.method == 'POST':
+        source = request.form.get('source_lang')
+        dict_il_tl_result = il_smt_trans(source)
+        dict_il_tl_result = pd.DataFrame(dict_il_tl_result)
+        op_sen_list = dict_il_tl_result['System Output'].tolist()
+        expected = request.form.get('expected_out')
+        expected = dict_il_tl_result['Expected Output'].tolist()
+        ave_bleu = scoring_bleu(dict_il_tl_result)
+        ave_ter = ter(dict_il_tl_result)
+
+    return render_template('system_tester_il-tg.html', source=source, op_sen_list=op_sen_list, expected=expected, ave_bleu=ave_bleu, ave_ter=ave_ter)
 
 @views.route('/home', methods=['GET', 'POST'])
 def landing_page():
